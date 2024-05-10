7f8048ed1eaa62bf3139baa8ad6f7ab30c19075b
Rupal Mittal <rmittal@ccsfundraising.com>
2024-05-10 21:04:51 +0000

    added pred_mod.R that calls pred_mod.py

---
+++ pred_mod.R
@@ -0,0 +1,18 @@
+library(reticulate)
+
+# Set the path to your virtual environment
+virtualenv_path <- "/home/RMittal@ccsfundraising.com/ccs_pred_mod/.py39_scikit"
+
+# Set the Python executable path within the virtual environment
+python_executable <- file.path(virtualenv_path, "bin", "python")
+
+# Define the command to call the Python script with arguments
+python_script <- paste(python_executable, "ft_nsf.py -v 1", sep = " ")
+
+# Execute the command
+system(python_script)
+
+# library(reticulate)
+# virtualenv_path <- "/path/to/myenv"
+# virtualenv_install(virtualenv_path, "chardet")
+
